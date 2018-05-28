7af3d9281ed6b9eaf8e1b7ff6ef4041e15b0a48d
ebitter <991807771@qq.com>
2018-05-28 16:46:14 +0800

    base4

---
+++ main.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <vector>
+
+int main(int argc, char *argv[])
+{
+
+    std::vector<int> src;
+    for(int i=1; i<101; i++)
+    {
+        src.push_back(i);
+    }
+    for(int i=0; i< src.size(); i++)
+        std::cout << "old: "<< src[i] << std::endl;
+
+
+    src.erase(src.begin()+91, src.end());
+    for(int i=0; i< src.size(); i++)
+        std::cout << "erase last 9:" << src[i] << std::endl;
+}
